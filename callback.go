1550c2a8c8def30d6fcb44e8a42f32036dd9e91f
zonesan <san@zoobook.org>
2017-04-19 11:32:52 +0800

    debug.

--- callback.go
+++ callback.go
@@ -21,7 +21,7 @@ func handleGitHubCallback(w http.ResponseWriter, r *http.Request, _ httprouter.P
 		return
 	}
 
-	clog.Debug("save hub access token '%v'@'%v'", user, token)
+	clog.Debugf("save hub access token '%v'@'%v'", user, token.AccessToken)
 	if err := saveGitHubToken(store, user, token); err != nil {
 		clog.Error("save github token error:", err)
 	}
@@ -42,7 +42,7 @@ func handleGitLabCallback(w http.ResponseWriter, r *http.Request, _ httprouter.P
 		return
 	}
 
-	clog.Debug("save lab access token '%v'@'%v'", user, token)
+	clog.Debugf("save lab access token '%v'@'%v'", user, token.AccessToken)
 	if err := saveGitLabToken(store, user, token); err != nil {
 		clog.Error("save gitlab token error:", err)
 	}
